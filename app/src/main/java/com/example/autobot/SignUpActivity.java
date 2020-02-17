1b1aa2efe2a22086b70efbd4053be6f52b9d7f88
xuechun <xqiu1@ualberta.ca>
2020-02-17 14:25:59 -0700

    sign up activity

--- app/src/main/java/com/example/autobot/SignUpActivity.java
+++ app/src/main/java/com/example/autobot/SignUpActivity.java
@@ -3,7 +3,10 @@ package com.example.autobot;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Checkable;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,9 @@ public class SignUpActivity extends AppCompatActivity {
         EditText editTextPhoneNumber = findViewById(R.id.accountPhoneNumber);
         EditText editTextUserName = findViewById(R.id.accountUserName);
         Button signUpButton = findViewById(R.id.signUpButton);
+        RadioButton radioButtonDriver = findViewById(R.id.radioButtonDriver);
+        RadioButton radioButtonRider = findViewById(R.id.radioButtonRider);
+        CheckBox checkBoxPolicy = findViewById(R.id.checkBoxPolicy);
 
 
 
