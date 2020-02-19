ac160efa349e642020d5aaba3d28fbed69232ab8
xuechun <xqiu1@ualberta.ca>
2020-02-19 11:51:36 -0700

    drawer and fragments

--- app/src/main/java/com/example/autobot/LoginActivity.java
+++ app/src/main/java/com/example/autobot/LoginActivity.java
@@ -1,4 +1,29 @@
 package com.example.autobot;
 
-public class LoginActivity {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+
+        final Intent intent = getIntent();
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentHomePage = new Intent(LoginActivity.this, HomePageActivity.class);
+                startActivity(intentHomePage);
+            }
+        });
+    }
 }
