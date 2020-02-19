b3285ca2ca284031719d3a821eb00f96c7cc3a1f
xuechun <xqiu1@ualberta.ca>
2020-02-18 19:44:06 -0700

    some modifications

--- app/src/main/java/com/example/autobot/SetPasswordActivity.java
+++ app/src/main/java/com/example/autobot/SetPasswordActivity.java
@@ -1,7 +1,10 @@
 package com.example.autobot;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,19 @@ public class SetPasswordActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_password_activity);
-        Intent intent = getIntent();
+
+        Button buttonConfirmPassword = findViewById(R.id.buttonConfirmPassword);
+
+        final Intent intent = getIntent();
+
+        buttonConfirmPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentHomePage = new Intent(SetPasswordActivity.this, HomePageActivity.class);
+                startActivity(intentHomePage);
+            }
+        });
     }
+
+
 }
