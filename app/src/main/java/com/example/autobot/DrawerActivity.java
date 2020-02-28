a63452b56b649bac362edd7a5bde8d5ec73b3db3
xuechun <xqiu1@ualberta.ca>
2020-02-27 20:13:07 -0700

    update yiping's merge

--- app/src/main/java/com/example/autobot/DrawerActivity.java
+++ app/src/main/java/com/example/autobot/DrawerActivity.java
@@ -17,7 +17,6 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
-@SuppressLint("Registered")
 public class DrawerActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
 
