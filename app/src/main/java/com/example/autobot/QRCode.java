4cbb7b7ec50ecda36839e7ff210a14cf760cc973
xuechun <xqiu1@ualberta.ca>
2020-03-02 16:29:52 -0700

    delete somthing in app/gradle

--- app/src/main/java/com/example/autobot/QRCode.java
+++ app/src/main/java/com/example/autobot/QRCode.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
-
 public class QRCode extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
