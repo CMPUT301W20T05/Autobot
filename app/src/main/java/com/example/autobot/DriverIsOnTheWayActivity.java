64797372b2f8160f06e98e752f796e4df01fa8ed
xuechun <xqiu1@ualberta.ca>
2020-03-01 17:33:35 -0700

    driver is on the way activity

---
+++ app/src/main/java/com/example/autobot/DriverIsOnTheWayActivity.java
@@ -0,0 +1,68 @@
+package com.example.autobot;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class DriverIsOnTheWayActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Rider Mode");
+        View rootView = getLayoutInflater().inflate(R.layout.cancel_ride, frameLayout);
+
+        TextView textViewDriverCondition = findViewById(R.id.driver_condition);
+        Button buttonSeeProfile = findViewById(R.id.see_profile);
+        ImageButton imageButtonPhone = findViewById(R.id.phoneButton);
+        ImageButton imageButtonEmail = findViewById(R.id.emailButton);
+        Button buttonCancelOrder = findViewById(R.id.cancel_order);
+
+        final String phoneNumber = null;
+
+        //make a phone call to driver
+        imageButtonPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + phoneNumber));//change the number.
+                if (ActivityCompat.checkSelfPermission(DriverIsOnTheWayActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(callIntent);
+
+            }
+        });
+
+        buttonCancelOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //cancel order
+            }
+        });
+
+        //when driver arrived, show notification
+        sendOnChannel();
+    }
+}
