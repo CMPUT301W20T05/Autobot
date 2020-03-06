f8eb756bba29314d88b3905e75e34090b300833a
xuechun <xqiu1@ualberta.ca>
2020-03-05 17:57:44 -0700

    tests

---
+++ app/src/androidTest/java/com/example/autobot/SignUpActivityTest.java
@@ -0,0 +1,55 @@
+package com.example.autobot;
+
+import android.app.Activity;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class SignUpActivityTest {
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception{
+        //This method used to create the solo object with instrumentation and activity as arguments
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
+
+    @Test
+    public void checkActivityChange() {
+        // Asserts that the current activity is the signupactivity. Otherwise, show wrong message
+        solo.assertCurrentActivity("Wrong Activity", SignUpActivity.class);
+    }
+
+    /**
+     * Check to see if the back button works or not
+     */
+    @Test
+    public void checkBackButton() {
+        solo.clickOnButton("Continue");
+        //assert current activity is mainActivity
+        solo.assertCurrentActivity("Not in MainActivity", MainActivity.class);
+    }
+
+    /**
+     * Closes the activity after each test
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
+}
