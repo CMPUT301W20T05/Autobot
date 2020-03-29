4398d87ea904f68911a3b1012a96ef37ad70ca62
Zonglun Li <zonglun1@ualberta.ca>
2020-03-29 16:11:53 -0600

    protect19

--- app/src/main/java/com/example/autobot/LoginActivity.java
+++ app/src/main/java/com/example/autobot/LoginActivity.java
@@ -173,25 +173,52 @@ public class LoginActivity extends AppCompatActivity {
                                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                                     @Override
                                     public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                        if (documentSnapshot.exists()){
+                                        if (documentSnapshot.exists()) {
                                             String userName = Account; // set username to username
                                             String RightPassword = documentSnapshot.getString("Password");
                                             String Type = documentSnapshot.getString("Type");
-                                            if (RightPassword.equals(Password)) {
-                                                if (Type.equals("Rider")) {
-                                                    Intent intentHomePage = new Intent(LoginActivity.this, HomePageActivity.class);
-                                                    intentHomePage.putExtra("User",Account);
-                                                    finish();
-                                                    startActivity(intentHomePage);
+                                            user = new User(userName);
+                                            DocumentReference documentReference = db.collectionReference_user.document(userName);
+                                            documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                    DocumentSnapshot document = task.getResult();
+                                                    user.setEmailAddress((String) document.get("EmailAddress"));
+                                                    user.setFirstName((String) document.get("FirstName"));
+                                                    user.setLastName((String) document.get("LastName"));
+                                                    double Lat = Double.valueOf((String) document.get("CurrentLocationLat"));
+                                                    Log.d("Testing", (String) document.get("CurrentLocationLat"));
+                                                    double Lnt = Double.valueOf((String) document.get("CurrentLocationLnt"));
+                                                    LatLng CurrentLocation = new LatLng(Lat, Lnt);
+                                                    user.updateCurrentLocation(CurrentLocation);
+                                                    user.setEmergencyContact((String) document.get("EmergencyContact"));
+                                                    user.setHomeAddress((String) document.get("HomeAddress"));
+                                                    user.setPassword((String) document.get("Password"));
+                                                    user.setPhoneNumber((String) document.get("PhoneNumber"));
+                                                    user.setStars(Double.valueOf((String) document.get("StarsRate")));
+                                                    user.setUserType((String) document.get("Type"));
+                                                    user.setUsername((String) document.get("Username"));
+                                                    String uri = ((String) document.get("ImageUri"));
+                                                    user.setUri(uri);
+                                                    user.setGoodRate((String) document.get("GoodRate"));
+                                                    user.setBadRate((String) document.get("BadRate"));
+                                                    Log.d("Testing", user.getUserType() + "hihih");
+                                                    //save user in shareprefence, don't need to login when you reopen the app
+                                                    save_user_login();
+                                                    if (RightPassword.equals(Password)) {
+                                                        if (Type.equals("Rider")) {
+                                                            Intent intentHomePage = new Intent(LoginActivity.this, HomePageActivity.class);
+                                                            intentHomePage.putExtra("User", Account);
+                                                            startActivity(intentHomePage);
+                                                        } else {
+                                                            Intent intentHomePage = new Intent(LoginActivity.this, DriverhomeActivity.class);
+                                                            intentHomePage.putExtra("User", Account);
+                                                            startActivity(intentHomePage);
+                                                        }
+                                                    } else
+                                                        editTextInputPassword.setError("The Wrong password!");
                                                 }
-                                                else {
-                                                    Intent intentHomePage = new Intent(LoginActivity.this, DriverhomeActivity.class);
-                                                    intentHomePage.putExtra("User",Account);
-                                                    finish();
-                                                    startActivity(intentHomePage);
-                                                }
-                                            }
-                                            else editTextInputPassword.setError("The Wrong password!");
+                                            });
                                         }
                                         else {
                                             editAccount.setError("User name is not exist!");
