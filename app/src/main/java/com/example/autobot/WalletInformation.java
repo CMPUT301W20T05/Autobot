3b8f7d4b22b03c115e9645988638d82113252672
Weida-W <weidaaa@outlook.com>
2020-03-28 20:26:50 -0600

    Wallet

---
+++ app/src/main/java/com/example/autobot/WalletInformation.java
@@ -0,0 +1,37 @@
+package com.example.autobot;
+
+public class WalletInformation {
+    String Destination;
+    String Cost;
+    String date;
+
+    public WalletInformation(String destination, String cost, String date) {
+        Destination = destination;
+        Cost = cost;
+        this.date = date;
+    }
+
+    public String getDestination() {
+        return Destination;
+    }
+
+    public void setDestination(String destination) {
+        Destination = destination;
+    }
+
+    public String getCost() {
+        return Cost;
+    }
+
+    public void setCost(String cost) {
+        Cost = cost;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
