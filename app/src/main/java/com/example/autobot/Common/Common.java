743934e7c3b8b417b382f0cae1fa4bde74b68237
YipingPeng <43100360+YipingPeng@users.noreply.github.com>
2020-03-30 21:21:52 -0600

    update

--- app/src/main/java/com/example/autobot/Common/Common.java
+++ app/src/main/java/com/example/autobot/Common/Common.java
@@ -48,16 +48,53 @@ public class Common {
             }
         });
         //Collections.sort(historyRequests, Collections.reverseOrder());
+
         return historyRequests;
     }
     public static ArrayList<HistoryRequest> sortListByStatus(ArrayList<HistoryRequest> historyRequests){
-        Collections.sort(historyRequests, new Comparator<HistoryRequest>() {
-            @Override
-            public int compare(HistoryRequest historyRequest, HistoryRequest t1) {
-                return historyRequest.getStatus().compareTo(t1.getStatus());
+//        Collections.sort(historyRequests, new Comparator<HistoryRequest>() {
+//            @Override
+//            public int compare(HistoryRequest historyRequest, HistoryRequest t1) {
+//                return historyRequest.getStatus().compareTo(t1.getStatus());
+//            }
+//        });
+        ArrayList<HistoryRequest> newHistoryRequest = new ArrayList<>();
+        for (int i = 0;i < historyRequests.size()-1;i++){
+            if (historyRequests.get(i).getStatus().toString().equals("Current")){
+                newHistoryRequest.add(historyRequests.get(i));
             }
-        });
-        return historyRequests;
+        }
+        for (int i = 0;i < historyRequests.size()-1;i++){
+            if (historyRequests.get(i).getStatus().toString().equals("Request Sending")){
+                newHistoryRequest.add(historyRequests.get(i));
+            }
+        }
+        for (int i = 0;i < historyRequests.size()-1;i++){
+            if (historyRequests.get(i).getStatus().toString().equals("Driver Accepted")){
+                newHistoryRequest.add(historyRequests.get(i));
+            }
+        }
+        for (int i = 0;i < historyRequests.size()-1;i++){
+            if (historyRequests.get(i).getStatus().toString().equals("Rider Accepted")){
+                newHistoryRequest.add(historyRequests.get(i));
+            }
+        }
+        for (int i = 0;i < historyRequests.size()-1;i++){
+            if (historyRequests.get(i).getStatus().toString().equals("Rider picked")){
+                newHistoryRequest.add(historyRequests.get(i));
+            }
+        }
+        for (int i = 0;i < historyRequests.size()-1;i++){
+            if (historyRequests.get(i).getStatus().toString().equals("Trip Completed")){
+                newHistoryRequest.add(historyRequests.get(i));
+            }
+        }
+        for (int i = 0;i < historyRequests.size()-1;i++){
+            if (historyRequests.get(i).getStatus().toString().equals("Cancel")){
+                newHistoryRequest.add(historyRequests.get(i));
+            }
+        }
+        return newHistoryRequest;
     }
 
 
